d8f150544ce7f78763ac7cf5125f8e39005915eb
yaseenomer <yaseeno373@gmail.com>
2026-02-03 20:41:54 +0200

    implement some imbrove

--- src/ws/server.js
+++ src/ws/server.js
@@ -19,9 +19,33 @@ export function attachWebSocketServer(server) {
     maxPayload: 1024 * 1024,
   });
   wss.on("connection", (socket) => {
+    socket.isAlive = true;
+    socket.on("pong", () => {
+      socket.isAlive = true;
+    });
+
+    socket.on("message", (data) => {
+      handleMessage(socket, data);
+    });
+
+    socket.on("error", () => {
+      socket.terminate();
+    });
+
     sendJson(socket, { type: "welcome" });
     socket.on("error", console.error);
   });
+
+  const interval = setInterval(() => {
+    wss.clients.forEach((ws) => {
+      if (ws.isAlive === false) return ws.terminate();
+
+      ws.isAlive = false;
+      ws.ping();
+    });
+  }, 30000);
+
+  wss.on("close", () => clearInterval(interval));
 
   function broadcastMatchCreated(match) {
     broadcast(wss, { type: "match_created", data: match });
